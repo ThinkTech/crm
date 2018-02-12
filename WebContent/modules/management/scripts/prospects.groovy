c1641c6e10ab2c9c0c2486e913fc9d3b51a0c0f1
Mamadou Lamine Ba <lamine.ba@thinktech.sn>
2018-02-12 15:44:17 +0000

    improving the project.

--- WebContent/modules/management/scripts/prospects.groovy
+++ WebContent/modules/management/scripts/prospects.groovy
@@ -24,10 +24,10 @@ class ModuleAction extends ActionSupport {
     def getProspectInfo() {
 	    def id = getParameter("id")
 	   def connection = getConnection()
-	   def user = connection.firstRow("select u.*, s.name as structure from users u, structures s where u.id = ? and u.structure_id = s.id", [id])
-	   user.createdOn = new SimpleDateFormat("dd/MM/yyyy - HH:mm:ss").format(user.createdOn)
+	   def prospect = connection.firstRow("select * from prospects where id = ?", [id])
+	   prospect.createdOn = new SimpleDateFormat("dd/MM/yyyy - HH:mm:ss").format(prospect.createdOn)
 	   connection.close()
-	   json([entity : user])
+	   json([entity : prospect])
 	}
     	
 	def getConnection() {
