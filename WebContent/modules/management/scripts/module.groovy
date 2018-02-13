7eb211d78db6357552ebb83085f1ff60bb536fc2
Mamadou Lamine Ba <lamine.ba@thinktech.sn>
2018-02-13 19:06:03 +0000

    improving the project.

--- WebContent/modules/management/scripts/module.groovy
+++ WebContent/modules/management/scripts/module.groovy
@@ -16,7 +16,7 @@ class ModuleAction extends ActionSupport {
           project.progression = row.progression
           projects << project
        })
-       def projects_count = connection.firstRow("select count(*) AS num from projects where status = 'stand by' or status = 'in progress'").num
+       def projects_count = connection.firstRow("select count(*) AS num from projects where status = 'stand by'").num
        def tickets_unsolved = connection.firstRow("select count(*) AS num from tickets where status != 'finished'").num
        def bills_count = connection.firstRow("select count(*) AS num from bills b, projects p where b.project_id = p.id and b.status = 'stand by'").num
        connection.close() 
