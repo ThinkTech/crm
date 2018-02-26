b08a8042c9ea357fbda5d035b4399c52be66a8bd
Mamadou Lamine Ba <lamine.ba@thinktech.sn>
2018-02-26 22:30:12 +0000

    improving the project.

--- WebContent/modules/payment/scripts/module.groovy
+++ WebContent/modules/payment/scripts/module.groovy
@@ -5,7 +5,7 @@ import app.FileManager
 
 class ModuleAction extends ActionSupport {
 
-   def pay(){
+    def pay(){
       def bill = parse(request) 
       def connection = getConnection()
 	  connection.executeUpdate "update bills set code = ?, status = 'finished', paidWith = ?, paidOn = NOW(), paidBy = ? where id = ?", [bill.code,bill.paidWith,session.getAttribute("user").id,bill.id]
@@ -13,8 +13,8 @@ class ModuleAction extends ActionSupport {
 	  	connection.executeUpdate "update projects set status = 'in progress', progression = 10 where id = ?", [bill.project_id]
 	  	def project = connection.firstRow("select * from projects  where id = ?", [bill.project_id])
 	  	def info = "le paiement de la caution a &edot;t&edot; &edot;ffectu&edot; et le contrat vous liant &aacute; ThinkTech a &edot;t&edot; g&edot;n&edot;r&edot; et ajout&edot; aux documents du projet"
-	  	connection.executeUpdate "update projects_tasks set date = ?, status = 'finished', info = ? , progression = 100 where task_id = ? and project_id = ?", [project.date,info,1,bill.project_id]
-	  	connection.executeUpdate "update projects_tasks set date = ?, status = 'in progress' where task_id = ? and project_id = ?", [project.date,2,bill.project_id]
+	  	connection.executeUpdate "update projects_tasks set date = ?, status = 'finished', info = ? , progression = 100 where name = ? and project_id = ?", [project.date,info,"Contrat et Caution",bill.project_id]
+	  	connection.executeUpdate "update projects_tasks set date = ?, status = 'in progress' where name = ? and project_id = ?", [project.date,"Traitement",bill.project_id]
 	  	def params = ["contrat.doc",50000,bill.project_id,session.getAttribute("user").id]
 	    connection.executeInsert 'insert into documents(name,size,project_id,createdBy) values (?,?,?,?)',params
 	  	generateContract(project)
