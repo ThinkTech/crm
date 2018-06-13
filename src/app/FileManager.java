c3b331aac085d8ed4984bc5a8c7ece688c112df7
ThinkTechSN <dev@thinktech.sn>
2018-06-13 19:15:05 +0000

    improving the project.

--- src/app/FileManager.java
+++ src/app/FileManager.java
@@ -2,18 +2,17 @@ package app;
 
 import java.io.InputStream;
 import java.io.OutputStream;
-
 import com.dropbox.core.DbxRequestConfig;
 import com.dropbox.core.v2.DbxClientV2;
 import com.dropbox.core.v2.files.WriteMode;
 
 public class FileManager {
 	
-    private static final String ACCESS_TOKEN = "pQXsY7k7n6AAAAAAAAAACCeARGCUI6ZaOF4vho8GFb39O6lv-8wWo2LqYRm1ShIY";
+    private static final String ACCESS_TOKEN = "pQXsY7k7n6AAAAAAAAAAPWBEAjxZy5Xcgtsd8kyz9VsKOIrEzorICsW29BV4bspM";
     private DbxClientV2 client;
     
     public FileManager(){
-    	DbxRequestConfig config = new DbxRequestConfig("dropbox/thinktech-portal");
+    	DbxRequestConfig config = new DbxRequestConfig("dropbox/thinktech-app");
         client = new DbxClientV2(config, ACCESS_TOKEN);
     }
     
