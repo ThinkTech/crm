c5afcbadf2908191456c20c9eea1390e543b8dc1
ThinkTech <dev@thinktech.sn>
2018-06-27 11:33:39 +0000

    improving the project.

--- WebContent/modules/management/scripts/domains.groovy
+++ WebContent/modules/management/scripts/domains.groovy
@@ -54,9 +54,9 @@ class ModuleAction extends ActionSupport {
 	    def connection = getConnection()
 	    connection.executeUpdate "update domains set status = 'finished', registeredOn = Now() where id = ?", [domain.id] 
 	    def user = connection.firstRow("select * from users where id = ?", [domain.user_id])
-	    connection.close()
 	    sendMail(user.name,user.email,"Enregistrement du domaine ${domain.name} pour ${domain.year} an termin&eacute;",getRegistrationTemplate(domain))
-		json([status: 1])
+	    connection.close()
+	    json([status: 1])
 	}
 	
 	def activateMailOffer(){
@@ -64,7 +64,10 @@ class ModuleAction extends ActionSupport {
 	     def connection = getConnection()
 	     connection.executeUpdate "update domains set email = ?, emailActivatedOn = Now() where id = ?", [order.email,order.id]
 	     connection.executeUpdate "update tickets set progression = 100, status = 'finished', closedOn = NOW(), closedBy = ? where service = 'mailhosting' and product_id = ?", [user.id,order.id]
-	     connection.close()
+	     def user_id = connection.firstRow("select user_id from domains where id = ?", [order.id]).user_id
+	     def user = connection.firstRow("select * from users where id = ?", [user_id])
+	     sendMail(user.name,user.email,"Configuration email pour le domaine ${order.domain} termin&eacute;e",getEmailTemplate(order))
+		 connection.close()
 	     json([status: 1])
 	}
 
@@ -110,6 +113,45 @@ class ModuleAction extends ActionSupport {
 		 }
 		'''
 		def template = engine.createTemplate(text).make([domain:domain,url : "https://thinktech-app.herokuapp.com"])
+		template.toString()
+	}
+	
+	def getEmailTemplate(order) {
+		MarkupTemplateEngine engine = new MarkupTemplateEngine()
+		def text = '''\
+		 div(style : "font-family:Tahoma;background:#fafafa;padding-bottom:16px;padding-top: 25px"){
+		 div(style : "padding-bottom:12px;margin-left:auto;margin-right:auto;width:80%;background:#fff") {
+		    img(src : "https://www.thinktech.sn/images/logo.png", style : "display:block;margin : 0 auto")
+		    div(style : "margin-top:10px;padding-bottom:2%;padding-top:2%;text-align:center;background:#05d2ff") {
+		      h4(style : "font-size: 120%;color: #fff;margin: 3px") {
+		        span("La configuration de votre business email est termin&eacute;e")
+		      }
+		    }
+		    div(style : "width:90%;margin:auto;margin-top : 30px;margin-bottom:30px") {
+		     h5(style : "font-size: 90%;color: rgb(0, 0, 0);margin-bottom: 0px") {
+		         span("Plan : $order.plan")
+		     }
+		     h5(style : "font-size: 90%;color: rgb(0, 0, 0);margin-bottom: 0px") {
+		         span("Email : $order.email@$order.domain")
+		     }
+		     p("la configuration de votre business email est maintenant termin&eacute;. Cliquer sur le bouton Connecter pour consulter votre nouvelle messagerie ou installer l\'application mail Zoho sur votre smartphone.")
+
+		    }
+		    div(style : "text-align:center;margin-top:30px;margin-bottom:10px") {
+			    a(href : "https://mail.zoho.com",style : "font-size:130%;width:140px;margin:auto;text-decoration:none;background: #05d2ff;display:block;padding:10px;border-radius:2px;border:1px solid #eee;color:#fff;") {
+			        span("Connecter")
+			    }
+			}
+		  }
+		  
+		  div(style :"margin: 10px;margin-top:10px;font-size : 80%;text-align:center") {
+		      p("Vous recevez cet email parce que vous (ou quelqu\'un utilisant cet email)")
+		      p("a souscrit au service domainhosting en utilisant cette adresse")
+		  }
+		  
+		 }
+		'''
+		def template = engine.createTemplate(text).make([order:order])
 		template.toString()
 	}
 	
