fa617dedef4508f31a87a3147add5f68b105b673
ThinkTech <dev@thinktech.sn>
2018-06-29 13:15:09 +0000

    improving the project.

--- WebContent/modules/management/js/support.js
+++ WebContent/modules/management/js/support.js
@@ -2,12 +2,12 @@ $(document).ready(function(){
 	page.details.bind = function(container,ticket) {
 		$("[data-status='"+ticket.priority+"']",container).show();
 		if(ticket.status == "finished"){
-			$("legend a",container).hide();
-			$("a.refresh",container).hide();
+			$("a.refresh,legend a",container).hide();
 		}else if(ticket.status == "stand by"){
 			$(".priority-edit,.service-edit",container).show();
+			$("legend a,.close-ticket",container).hide();
 		}else{
-			$(".progression-edit",container).show();
+			$("legend a,.progression-edit",container).show();
 		}
 		if(ticket.comments.length) page.details.showComments(ticket.comments);
 		$(".messages form",container).submit(function(event){
@@ -96,52 +96,7 @@ $(document).ready(function(){
 	$(".window > div > form").submit(function(event){
 		    page.details.addTicket($(this));
 		    return false;
-	});
-	page.details.addTicket = function(form){
-		const ticket = {};
-		ticket.subject = form.find("input[name=subject]").val();
-		ticket.service =  form.find("select[name=service]").val();
-		ticket.priority =  form.find("select[name=priority]").val();
-		ticket.message =  tinyMCE.activeEditor.getContent();
-		if(tinyMCE.activeEditor.getContent({format: 'text'}).trim() == ""){
-			alert("vous devez entrer une description",function(){
-				tinyMCE.activeEditor.focus();
-			});
-			return false;
-		}
-		const date = new Date();
-		ticket.date = (date.getDate()>=10?date.getDate():("0"+date.getDate()))+"/"+(date.getMonth()>=10?(date.getMonth()+1):("0"+(date.getMonth()+1)))+"/"+date.getFullYear();
-		confirm("&ecirc;tes vous s&ucirc;r de vouloir cr&edot;&edot;r ce ticket?",function(){
-			page.form.hide();
-			page.wait({top : form.offset().top+200});
-			$.ajax({
-				  type: "POST",
-				  url: form.attr("action"),
-				  data: JSON.stringify(ticket),
-				  contentType : "application/json",
-				  success: function(response) {
-					  if(response.id){
-						  $("input[type=text]",form).val("");
-						  tinyMCE.activeEditor.setContent("");
-						  ticket.id = response.id;
-						  page.table.addRow(ticket,function(){
-							  page.release();
-							  alert("votre ticket a &edot;t&edot; bien cr&edot;&edot;");
-							  $.each($(".info-updates h3"),function(i,node){
-								  const h3 = $(node);
-								  h3.html(parseInt(h3.text())+1);
-							  });
-						  });
-					  }
-				  },
-				  error : function(){
-					  page.release();
-					  alert("erreur lors de la connexion au serveur");
-				  },
-				  dataType: "json"
-			});
-		});
-	  };
+	  });
 	  page.details.addComment = function(form) {
 		const comment = {};
 		comment.message =  tinyMCE.activeEditor.getContent();
