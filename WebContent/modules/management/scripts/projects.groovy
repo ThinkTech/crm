788897ff24ee4174cc57659f2ac170d460422df3
ThinkTech <dev@thinktech.sn>
2018-07-27 09:18:47 +0000

    improving the project.

--- WebContent/modules/management/scripts/projects.groovy
+++ WebContent/modules/management/scripts/projects.groovy
@@ -161,7 +161,7 @@ class ModuleAction extends ActionSupport {
 		    img(src : "https://www.thinktech.sn/images/logo.png", style : "display:block;margin : 0 auto")
 		    div(style : "margin-top:10px;padding-bottom:2%;padding-top:2%;text-align:center;background:#05d2ff") {
 		      h4(style : "font-size: 120%;color: #fff;margin: 3px") {
-		        span("votre projet est maintenant termin&eacute;")
+		        span("Traitement projet termin&eacute;")
 		      }
 		    }
 		    div(style : "width:90%;margin:auto;margin-top : 30px;margin-bottom:30px") {
@@ -223,7 +223,7 @@ class ModuleAction extends ActionSupport {
 		    img(src : "https://www.thinktech.sn/images/logo.png", style : "display:block;margin : 0 auto")
 		    div(style : "margin-top:10px;padding-bottom:2%;padding-top:2%;text-align:center;background:#05d2ff") {
 		      h4(style : "font-size: 120%;color: #fff;margin: 3px") {
-		        span("T&agrave;che en cours de traitement")
+		        span("Traitement T&agrave;che en cours")
 		      }
 		    }
 		    div(style : "width:90%;margin:auto;margin-top : 30px;margin-bottom:30px") {
