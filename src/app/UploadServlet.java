95a45edc0f1c7978f9679c805d5ad00daf5d526a
thinktech <dev@thinktech.sn>
2018-09-11 14:29:43 +0000

    improving the project.

--- src/app/UploadServlet.java
+++ src/app/UploadServlet.java
@@ -16,19 +16,17 @@ import org.apache.commons.fileupload.servlet.ServletFileUpload;
 public class UploadServlet extends HttpServlet {
 
 	public void doPost(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException {
-		String structure_id = request.getParameter("structure_id");
-		String project_id = request.getParameter("project_id");
-		String dir = "structure_"+structure_id+"/"+"project_"+project_id;
 		boolean isMultipart = ServletFileUpload.isMultipartContent(request);
 		if(isMultipart){
 			try {
+				String directory = "structure_"+request.getParameter("structure_id")+"/"+"project_"+request.getParameter("project_id");
 				ServletFileUpload upload = new ServletFileUpload();
 				FileItemIterator iter = upload.getItemIterator(request);
 				FileManager manager = new FileManager();
 				while(iter.hasNext()) {
 					FileItemStream item = iter.next(); 
 					String name = new File(item.getName()).getName();
-					manager.upload(dir+"/"+name,item.openStream());
+					manager.upload(directory+"/"+name,item.openStream());
 				}
 			}catch(Exception e){
 			}
