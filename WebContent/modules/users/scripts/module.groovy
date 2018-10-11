dfa70eef1be966a303ec690de2bc5631010cd673
thinktech <dev@thinktech.sn>
2018-10-11 16:59:57 +0000

    improving the project.

--- WebContent/modules/users/scripts/module.groovy
+++ WebContent/modules/users/scripts/module.groovy
@@ -1,13 +1,4 @@
 class ModuleAction extends ActionSupport {
-
-    def String execute(){
-       if(user){
-         request.collaborators = connection.rows("select u.id, u.name,a.activated as active,a.locked from users u, accounts a where u.structure_id = ? and u.owner = false and a.user_id = u.id", [user.structure.id])
-         SUCCESS
-       }else{
-         ERROR
-       }
-    }
     
 	def login() {
 	   def info = request.body 
@@ -46,83 +37,6 @@ class ModuleAction extends ActionSupport {
 	   }else {
 	   	json([status: 0])
 	   }
-	}
-	
-	def updateProfil() {
-	   def user = request.body
-	   def email = session.user.email
-	   if(user.email != email){
-	    if(connection.firstRow("select id from users where email = ?", [user.email])){
-	   	    json([status: 0])
-	   	    return
-	   	 }
-	   }
-	   connection.executeUpdate 'update users set name = ?, email = ?  where id = ?', [user.name,user.email,session.user.id]
-	   user = connection.firstRow("select * from users where id = ?", [session.user.id])
-	   user.structure = session.user.structure
-       session.user = user
-	   json([status: 1])
-	}
-	
-	def addCollaborator(){
-	   def user = request.body
-	   if(user.email == session.user.email){
-	     json([status : 0])
-	   }
-	   else if(connection.firstRow("select id from users where email = ?", [user.email])) {
-		 json([status : 0])
-	   }else{
-	      def structure_id = session.user.structure.id
-	      def alphabet = (('A'..'N')+('P'..'Z')+('a'..'k')+('m'..'z')+('2'..'9')).join()  
- 		  def n = 15 
- 		  user.password = new Random().with { (1..n).collect { alphabet[ nextInt( alphabet.length() ) ] }.join() }
- 		  user.activationCode = new Random().with { (1..n).collect { alphabet[ nextInt( alphabet.length() ) ] }.join() }
-	      def params = [user.email,user.email,user.password,"collaborateur",false,structure_id]
-          def result = connection.executeInsert 'insert into users(name,email,password,role,owner,structure_id) values (?,?,sha(?),?,?,?)', params
-          def id = result[0][0]
-          params = [user.activationCode,id]
-       	  connection.executeInsert 'insert into accounts(activation_code,user_id) values (?, ?)', params
-       	  sendMail(user.email,user.email,"Veuillez confirmer cette demande de collaboration",parseTemplate("collaboration",[user:user,url:baseUrl,name:session.user.name]))
-	   	  json([id : id])
- 	   }
-	}
-	
-	def inviteCollaborator(){
-	    def user = request.body
-	    def alphabet = (('A'..'N')+('P'..'Z')+('a'..'k')+('m'..'z')+('2'..'9')).join()
-	    def n = 15
-	    user.activationCode = new Random().with { (1..n).collect { alphabet[ nextInt( alphabet.length() ) ] }.join() }
- 		def params = [user.activationCode,user.id]
-       	connection.executeUpdate 'update accounts set activated = false,activation_code = ? where user_id = ?', params 
-	    sendMail(user.email,user.email,"Veuillez confirmer cette demande de collaboration",parseTemplate("collaboration",[user:user,url:baseUrl,name:session.user.name]))
-	   	json([status : 1])
-	}
-	
-	def removeCollaborator(){
-	   def id = request.id
-       connection.execute 'delete from users where id = ?',[id]
-       connection.execute 'delete from accounts where user_id = ?',[id]
-	   json([id : id])
-	}
-	
-	def getCollaboratorInfo(){
-	   def id = request.id
-	   def user = connection.firstRow("select u.*, a.activated, a.locked from users u, accounts a where u.id = ? and u.id = a.user_id", [id])
-	   user.active = user.activated ? "oui" : "non"
-	   user.locked = user.locked ? "oui" : "non"
-	   json(user)
-	}
-	
-	def lockAccount(){
-	    def user = request.body
-	    connection.executeUpdate 'update accounts set locked = true  where user_id = ?', [user.id] 
-		json([status: 1])
-	}
-	
-	def unlockAccount(){
-	    def user = request.body
-	    connection.executeUpdate 'update accounts set locked = false  where user_id = ?', [user.id] 
-		json([status: 1])
 	}
 	
 	def logout() {
